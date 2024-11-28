34a3da31431cd3e43fb4cd0866dbad81f4174deb
6704062616117 <s6704062616117@email.kmutnb.ac.th>
2024-11-28 18:23:59 +0700

    is good

---
+++ lab1minx.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+	int num10;
+	int num5;
+	if (num1>num2){
+		printf("num1 is greater than num2");
+	}
+	if (num2>num1){
+		printf("num2 is greater than num1");
+	}
+}
